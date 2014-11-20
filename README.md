4e60c917d684feaab4bd33bd24b361c6aeb13fed
Brian Zambrano <brianz@gmail.com>
2014-11-20 13:19:26 -0700

    Setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Servant
+
+A python package to build services/microservices.
+
+See the `examples` folder to see what a service server and client look like.
+ 
+**_ATTENTION!_**
+
+Transport protocol is still highly experimental and in flux. Eventually the transport payload should be standardized so that clients/servers can be authored in different languates.
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'servant',
+    packages = ['servant'],
+    version = '0.0.1',
+    description = 'A library for building services',
+    author='Brian Zambrano',
+    author_email='brianz@gmail.com',
+    url='https://github.com/brianz/servant',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Distributed Computing',
+    ]
+)
