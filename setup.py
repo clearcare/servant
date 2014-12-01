93bb028e4eceef3c7571b6b479dbd92a272ba0e1
Brian Zambrano <brianz@gmail.com>
2014-12-01 14:39:25 -0700

    Packaging updates

--- setup.py
+++ setup.py
@@ -8,6 +8,15 @@ setup(
     author='Brian Zambrano',
     author_email='brianz@gmail.com',
     url='https://github.com/brianz/servant',
+    install_requires=[
+        'requests>=2.4.3',
+        'schematics>=1.0-0',
+        'six>=1.8.0',
+        'xmltodict>=0.9.0',
+    ],
+    tests_require=[
+        'pytest>=2.6.4',
+    ],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
