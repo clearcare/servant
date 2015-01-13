038ac8bded7e54f5d119096fa01d90f117f04a9d
Brian Zambrano <brianz@gmail.com>
2015-01-12 17:59:57 -0700

    Add a pagination mixin

---
+++ servant/service/actions/mixins.py
@@ -0,0 +1,42 @@
+from .base import Action
+
+import servant.fields
+
+
+class PaginationField(servant.fields.ContainerField):
+    page = servant.fields.IntField(min_value=1)
+    page_size = servant.fields.IntField(min_value=1, max_value=500)
+    num_pages = servant.fields.IntField()
+    total_count = servant.fields.IntField()
+
+
+class PaginatedActionMixin(Action):
+    # Input controls...page is 1-based
+    page = servant.fields.IntField(default=1, min_value=1)
+    page_size = servant.fields.IntField(default=25, min_value=1, max_value=500)
+
+    # Output
+    paging = servant.fields.ModelField(PaginationField, in_response=True)
+
+    def init_paging(self):
+        # set an empty paging field
+        self.paging = PaginationField({
+            'page': self.page,
+            'page_size': self.page_size,
+        })
+
+    def get_pagination_indices(self, offset=0, limit=None):
+        # Since page is 1-based, bump it down
+        page = self.page - 1
+        offset = offset or (page * self.page_size)
+        limit = limit or (offset + self.page_size)
+        return (offset, limit)
+
+    def paginate(self, listing):
+        """Apply the pagination
+
+        Every service may paginate differently, so this method needs to be implemented
+        specficically for each service or action.
+
+        """
+        raise NotImplementedError('Subclasses must implement this method')
