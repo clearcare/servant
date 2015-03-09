74ac3730a425a51fd3f87602ab607c902f6fcdc2
Brian Zambrano <brianz@gmail.com>
2015-03-09 16:28:17 -0600

    Set a hard version of schematics.
    Looks like the old 1.0-0 version went away

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url='https://github.com/brianz/servant',
     install_requires=[
         'requests>=2.4.3',
-        'schematics>=1.0-0',
+        'schematics==1.0.3',
         'six>=1.8.0',
         'xmltodict>=0.9.0',
     ],
