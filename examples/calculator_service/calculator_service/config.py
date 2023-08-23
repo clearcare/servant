ba0291503df660ed776ee662bba0f697cc902706
Matthew Ruston <mruston@clearcareonline.com>
2023-08-23 11:31:12 -0400

    delete examples to address Snyk vulns

--- examples/calculator_service/calculator_service/config.py
+++
@@ -1,3 +0,0 @@
-DUMMY_CONFIG = True
-VERSION = 1.0
-CALCULATOR_NAME = 'calculator_service'
--- examples/calculator_service/wsgi_handler.py
+++
@@ -1,4 +0,0 @@
-from calculator_service.service import CalculatorService
-
-service = CalculatorService()
-application = service.get_wsgi_application
