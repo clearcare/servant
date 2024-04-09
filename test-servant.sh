452510aa4bba93417945b3ea24bea6627897a80f
Charlie Ernesto <cernesto@clearcareonline.com>
2024-04-09 02:24:12 -0700

    fix

--- test-servant.sh
+++ test-servant.sh
@@ -4,10 +4,10 @@ sudo add-apt-repository universe
 sudo apt update
 sudo apt-get install -y python3.11
 #sudo apt-get install -y python3.11-pip
+#curl https://bootstrap.pypa.io/pip/get-pip.py --output get-pip.py
+#sudo python3.11 get-pip.py
 
-curl https://bootstrap.pypa.io/pip/3.11/get-pip.py --output get-pip.py
-sudo python3.11 get-pip.py
-
+python3.11 -m ensurepip
 
 ls /usr/bin/python*
 ls /usr/bin/pip*
@@ -22,8 +22,9 @@ echo "WORKING DIR $(pwd)"
 
 git fetch
 
+ls /usr/bin/*pytest*
+ls /usr/local/bin/*pytest*
+
 pytest --version
 pytest
-
-
 
