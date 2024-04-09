adfbc23ecf97877aa27071432de4d4a97ed8f0d9
Charlie Ernesto <cernesto@clearcareonline.com>
2024-04-09 02:16:34 -0700

    fix

--- test-servant.sh
+++ test-servant.sh
@@ -3,12 +3,17 @@
 sudo add-apt-repository universe
 sudo apt update
 sudo apt-get install -y python3.11
+#sudo apt-get install -y python3.11-pip
+
+curl https://bootstrap.pypa.io/pip/3.11/get-pip.py --output get-pip.py
+sudo python3.11 get-pip.py
+
 
 ls /usr/bin/python*
 ls /usr/bin/pip*
 
-python --version
-pip --version
+python3.11 --version
+pip3.11 --version
 
 pip install pytest
 pip install -r test_requirements.txt
